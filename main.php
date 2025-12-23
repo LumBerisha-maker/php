e5ec273314bbb66d3a617cc1b16054c786288697
LumBerisha-maker <lumberisha12@outlook.com>
2025-12-23 20:22:41 +0100

    moduli1

---
+++ main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 1</title>
+    <style>
+      h1{
+        color:red;
+      }
+
+
+
+    </style>
+</head>
+
+<body>
+    <h1>My first PHP</h1>
+
+    <?php
+     echo "Visca Barca"
+    ?>
+</body>
+</html>
