b2572eb48c8b9c9df400ebce216ea6ca91078850
LumBerisha-maker <lumberisha12@outlook.com>
2026-01-20 20:50:44 +0100

    module 3

---
+++ moduli3/index.php
@@ -0,0 +1,62 @@
+<?php 
+
+// $day = "tuesday";
+
+// switch($day) {
+//    case "Monday";
+//    echo ("Its Monday, start of the week");
+//    break;
+//    case "Tuesday";
+//    echo ("Its Tuesday, nothing");
+//    break;
+//    case "Wednesday";
+//    echo ("Its Wednesday, close");
+//    break;
+//    case "Thursday";
+//    echo ("Its Thursday, almost there");
+//    break;
+//    case "Friday";
+//    echo ("Its Friday, One stecase Monday";
+//     break
+//    case "Saturday";
+//    echo ("Its Saturday, Free day");
+//    break;
+//    case "Sunday";
+//    echo ("Its Sunday, start of Hell");
+//    break;
+//    default;
+//    echo "wrong"
+//    break;
+
+
+//    }
+
+$numri = 0;
+while ($numri <=5){
+    echo "the nummber is $numri <hr>";
+    $numri++;
+}
+
+
+$numri = 0;
+do{
+echo "the nummber is $numri <hr>";
+    $numri++;
+
+}while ($numri <=5);
+
+
+for($x=0; $x<=10; $x++){
+    echo "the nummber is $x <hr>";
+}
+
+
+
+
+
+
+
+
+
+
+?>
