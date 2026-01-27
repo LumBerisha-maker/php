a73ac9a1cdea30948bcb48219e4fecc4beeca424
LumBerisha-maker <lumberisha12@outlook.com>
2026-01-27 20:38:56 +0100

    moduli4

--- module4/index.php
+++ module4/index.php
@@ -10,7 +10,7 @@
 
 // echo gettype($x);
 // echo gettype($y);
-// echo gettype($z);
+// echo gettype($z);r
 
 
 // function printV(){
@@ -21,20 +21,82 @@
 // printV();
 
 
-function maximus($x, $y){
-    if ($x>$y){
-        return $x;
-    }else{
-        return$y;
-    }
+// function maximus($x, $y){
+//     if ($x>$y){
+//         return $x;
+//     }else{
+//         return$y;
+//     }
+// }
+
+// $test = maximus(100, 120);
+// echo $test;
+
+
+// function fully_divisible($n){
+//   if(($n % 2)==0){
+//     return "$n is fully divisble by 2!";}
+//   else{
+//     return "$n is not fully divisble by 2!";
+//   }
+// }
+// print_r (fully_divisible(67)."<hr>");
+// print_r (fully_divisible(7)."<hr>");
+// print_r (fully_divisible(8)."<hr>");
+// print_r (fully_divisible(2)."<hr>");
+
+
+// $x = 4;
+
+
+// function lumi(){
+//     $y = 4;
+//     echo $y;
+// }
+
+// lumi();
+// echo $x;
+
+
+$sports = array("Football","Gymnastic","Volleyball");
+
+$sport = ["gt3"];
+
+$number = array(67,6,7);
+
+for($i=0; $i<3 ;$i++){
+    echo $sports[$i],"<br>";
 }
 
-$test = maximus(100, 120);
-echo $test;
+array_pop($sports);
 
+var_dump($sports);
 
+//array_unshift adds an element
 
+array_unshift($sports, 'football');
 
+var_dump($sports)."<br>";
+
+//array_unshift deletes an element
+
+array_shift($sports);
+
+for($i=0;$i<4; $1+){
+    echo $sports[$i], "<br>";
+}
+
+//array sum
+var_dump(array_sum($numbers))
+
+$myString=[67,67];
+
+$mesatarja = array_sum($myString)/6;
+  echo $mesatarja;
 
 
 ?>
+
+
+
+
