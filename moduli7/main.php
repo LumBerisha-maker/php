a2697aba89ce3cf5626f87a24c99d0dcf060ce77
LumBerisha-maker <lumberisha12@outlook.com>
2026-02-10 20:35:34 +0100

    moduli7

---
+++ moduli7/main.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+
+
+try{
+
+
+$conn = new PDO("mysql:host=$host" , $user, $password);
+
+echo "connected";
+
+
+
+}catch(Exeption $e){
+    echo "not connected";
+}
+
+
+
+
+
+?>
